df198aa1810924586ed8db6acd19774118ca853f
rex-mcall <rexmcall@gmail.com>
2021-07-16 22:42:54 -0400

    Added documentation

--- experimental_code/tleToTrueAnomaly.py
+++ experimental_code/tleToTrueAnomaly.py
@@ -1,4 +1,9 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# 2021 Rex McAllister
+# https://downloads.rene-schwarz.com/download/M001-Keplerian_Orbit_Elements_to_Cartesian_State_Vectors.pdf
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Imports
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -40,6 +45,7 @@ TDO-4
 2 48620  26.1589 138.3041 2377990 101.4959 285.9800 10.88066450  6184
 """
 
+#parses the TLE into a usable format
 tle_lines1 = tle_string1.strip().splitlines()
 tle1 = TLE.from_lines(*tle_lines1)
 
@@ -49,35 +55,44 @@ tle2 = TLE.from_lines(*tle_lines2)
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # TLE to Keplerian Elements
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+#converts the longitude of the ascending node from TLE degrees to radians
 def calc_lan (tle) :
     lan = (tle.raan * toRad)
     return lan
 
+#converts the argument of perigee from TLE degrees to radians
 def calc_argp (tle) :
     argp = (tle.argp * toRad)
     return argp
 
+#converts the inclination from TLE degrees to radians
 def calc_inc (tle) :
     inc = (tle.inc * toRad)
     return inc
 
+#returns the native format of eccentricity from TLE
 def calc_eccentricity (tle) :
     ecc = (tle.ecc)
     return ecc
 
+#converts the mean motion from TLE rev/day to rad/s
 def calc_n (tle) :
     n = float(tle.n) * ( (1.0/86400.0) * (2.0 * pi) )
     return n
 
+#calculates the length of the semimajor axis in kilometers
 def calc_sma (tle) :
     n = calc_n(tle)
     sma = (gravParam / (n ** 2)) ** (1.0/3.0)
     return sma
 
+#converts the mean anomaly from TLE degrees to radians
 def calc_M (tle) :
     M = tle.M * toRad
     return M
 
+#calculates the eccentric anomaly (rad) using a Newton-Rhapson setup
 def calc_E (tle) :
     M = calc_M(tle)
     nextE = M
@@ -87,23 +102,17 @@ def calc_E (tle) :
         nextE = (( (M) - (ecc * (E * math.cos(E) - math.sin(E))) ) / (1 - ecc * math.cos(E)))
     return E
 
+#calculates the true anomaly (rad)
 def calc_truea (tle) :
     M = calc_M(tle)
     E = calc_E(tle)
     ecc = calc_eccentricity(tle)
-
-    # M + (n * deltaT)
-
-    nextE = M
-
-    while ( abs((nextE - E) / nextE) > 0.00001) :
-        E = nextE
-        nextE = (( (M) - (ecc * (E * math.cos(E) - math.sin(E))) ) / (1 - ecc * math.cos(E)))
     
     truea = 2 * atan2(sqrt(1 + ecc) * sin(E / 2), sqrt(1 - ecc) * cos(E / 2))
 
     return truea
 
+#creates an array of the value of true anomaly for a period of time over the orbit
 def calc_truea_time(tle, increment, numSeconds) :
     E = calc_E(tle)
     M = calc_M(tle)
@@ -116,6 +125,7 @@ def calc_truea_time(tle, increment, numSeconds) :
             yield start
             start += step
     for i in my_range (0, numSeconds, 60):
+        #same as calc_E except is propogates the mean anomaly forward in time
         nextE = M + (n * i)
 
         while ( abs((nextE - E) / nextE) > 0.00001) :
@@ -131,6 +141,7 @@ def calc_truea_time(tle, increment, numSeconds) :
         trueaY.append(2 * atan2(sqrt(1 + ecc) * sin(E / 2), sqrt(1 - ecc) * cos(E / 2)))
     return epochX, trueaY
 
+#returns a dictionary of calculated orbital elements for a TLE
 def get_tle_to_kepler (tle) :
     keplerianElements = {
         "longAscNode"      : calc_lan(tle)          ,
