f858070dd54cec5ed148d6233dcb76ddd4bd8668
rex-mcall <rexmcall@gmail.com>
2021-07-19 18:09:21 -0400

    Question confirmed

--- experimental_code/intertialToRotationalFrame.py
+++ experimental_code/intertialToRotationalFrame.py
@@ -1,15 +1,18 @@
 from math import *
 
-def epochToJulianTime (tle) :
+
+def epochToJulianTime(tle):
     return tle.epoch.jd
 
-def julianToGMST (julDate) :
+
+def julianToGMST(julDate):
     tUT1 = (
         (julDate - 2451545.0) / 36525
     )
 
-    thetaGMST = ( #Grenwich mean siderial time???
-        67310.5841 + ((876600 + 8640184.212566) * tUT1) + (0.093104 * (tUT1 ** 2)) - (6.2e-6 * (tUT1 ** 3))
+    thetaGMST = (  # Grenwich mean siderial time
+        67310.5841 + ((876600 + 8640184.212566) * tUT1) + \
+        (0.093104 * (tUT1 ** 2)) - (6.2e-6 * (tUT1 ** 3))
     )
 
     angleSecToTimeSec = 360 - ((thetaGMST % 86400) / 240)
