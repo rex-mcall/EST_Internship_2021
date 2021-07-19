368c79549b1e91c19f4343f93e63bee21ad8448a
rex-mcall <rexmcall@gmail.com>
2021-07-19 18:06:06 -0400

    First conversion to inertial frame

--- experimental_code/cartOrbitalToIntertial.py
+++ experimental_code/cartOrbitalToIntertial.py
@@ -1,7 +1,27 @@
-import experimental_code.keplerToCartesianOrbital as ktco
+import keplerToCartesianOrbital as ktco
+import tleToTrueAnomaly as ttte
+import tleData
 from math import *
 import matplotlib.pyplot as plt
 
 
-def getInterialFramePoints (tle, xy) :
-    orbitalX, orbitalY = xy #orbital frame coords
+def getInterialFramePoints(tle, xy):
+    orbX, orbY = xy  # orbital frame coords
+    lan, argp, inc, ecc, n, M, a, E, v = ttte.tleToKepler(tle)
+
+    inertX = (
+        (orbX * ((cos(argp) * cos(lan)) - (sin(argp) * cos(inc) * sin(lan)))) -
+        (orbY * ((sin(argp) * cos(lan)) - (cos(argp) * cos(inc) * sin(lan))))
+    )
+
+    inertY = (
+        (orbX * ((cos(argp) * sin(lan)) + (sin(argp) * cos(inc) * cos(lan)))) +
+        (orbY * ((cos(argp) * cos(inc * cos(lan)) - (sin(argp) * sin(lan)))))
+    )
+
+    inertZ = (
+        (orbX * (sin(argp) * sin(inc))) +
+        (orbY * (cos(argp) * cos(inc)))
+    )
+
+    return inertX, inertY, inertZ
