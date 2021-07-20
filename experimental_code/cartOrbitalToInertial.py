fb5cde9cecde3080e936195f5b379375d5358357
rex-mcall <rexmcall@gmail.com>
2021-07-19 22:02:38 -0400

    Calculating position for an array of points

--- experimental_code/cartOrbitalToInertial.py
+++ experimental_code/cartOrbitalToInertial.py
@@ -4,11 +4,13 @@ import tleData
 from math import *
 import matplotlib.pyplot as plt
 
-
+# takes a set of points in the orbital frame and converts them to a 
+# 3d vector in the inertial frame oriented with Aries and equatorial plane
 def getInterialFramePoints(tle, xy):
     orbX, orbY = xy  # orbital frame coords
     lan, argp, inc, ecc, n, M, a, E, v = ttte.tleToKepler(tle)
 
+    # inertial frame coords
     inertX = (
         (orbX * ((cos(argp) * cos(lan)) - (sin(argp) * cos(inc) * sin(lan)))) -
         (orbY * ((sin(argp) * cos(lan)) - (cos(argp) * cos(inc) * sin(lan))))
@@ -25,3 +27,19 @@ def getInterialFramePoints(tle, xy):
     )
 
     return inertX, inertY, inertZ
+
+#tuple of orbital positions X and Y arrays to arrays of inertial XYZ
+def inertialFramePointsTime(tle, orbXY) :
+    orbX, orbY = orbXY
+    inertialX = []
+    inertialY = []
+    inertialZ = []
+
+    for i in range(0, len(orbX)):
+        x, y, z = getInterialFramePoints(tle, (orbX[i], orbY[i]))
+        inertialX.append(x)
+        inertialY.append(y)
+        inertialZ.append(z)
+
+    return inertialX, inertialY, inertialZ
+
