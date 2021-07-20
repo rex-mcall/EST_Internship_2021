22ff30f10b665c83192fb5827707440a0f184245
rex-mcall <rexmcall@gmail.com>
2021-07-19 20:40:43 -0400

    renamed files

--- experimental_code/inertialToRotationalFrame.py
+++ experimental_code/inertialToRotationalFrame.py
@@ -11,8 +11,7 @@ def julianToGMST(julDate):
     )
 
     thetaGMST = (  # Grenwich mean siderial time
-        67310.5841 + ((876600 + 8640184.212566) * tUT1) + \
-        (0.093104 * (tUT1 ** 2)) - (6.2e-6 * (tUT1 ** 3))
+        67310.5841 + ((876600 + 8640184.212566) * tUT1) + (0.093104 * (tUT1 ** 2)) - (6.2e-6 * (tUT1 ** 3))
     )
 
     angleSecToTimeSec = 360 - ((thetaGMST % 86400) / 240)
