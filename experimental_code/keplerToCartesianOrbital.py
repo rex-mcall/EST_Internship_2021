dd87bdca1abf92bc21631456093c168856b8ede6
rex-mcall <rexmcall@gmail.com>
2021-07-19 20:41:25 -0400

    moved test graph code to new file for organization

--- experimental_code/keplerToCartesianOrbital.py
+++ experimental_code/keplerToCartesianOrbital.py
@@ -3,24 +3,16 @@ import tleData
 from math import *
 import matplotlib.pyplot as plt
 
-orbitalPositionsX = []
-orbitalPositionsY = []
-dtcb = []  # distance to central body
-
 
 # uses TLE to return the current distance to the focus
 def distanceToCentralBody(tle, curr_trueA):
-    global dtcb
     lan, argp, inc, ecc, n, M, a, E, v = ttte.tleToKepler(tle)
     eccA = atan2(sqrt(1 - ecc ** 2) * sin(curr_trueA), ecc + cos(curr_trueA))
     eccA = eccA % (2 * pi)
     r = a * (1 - ecc * cos(eccA))
-    dtcb.append(r)  # distance to central body
     return r
 
 # calculates the cartesian coordinates for one point in time
-
-
 def getOrbitalCartesianCoords(tle, curr_trueA):
     r = distanceToCentralBody(tle, curr_trueA)
     x = r * cos(curr_trueA)
@@ -28,8 +20,6 @@ def getOrbitalCartesianCoords(tle, curr_trueA):
     return x, y
 
 # takes a tuple of true anomaly and epoch arrays and returns arrays of the calculated Cartesian coordinates over time
-
-
 def cartesianCoordsTime(tle, trueAnomalyTuple):
     epoch, trueAnomalyArray = trueAnomalyTuple
     orbitalPositionsX = []
@@ -41,44 +31,3 @@ def cartesianCoordsTime(tle, trueAnomalyTuple):
         orbitalPositionsY.append(cartCoordY)
 
     return orbitalPositionsX, orbitalPositionsY
-
-# Test code to plot points over time to make sure the functions work correctly
-
-
-def plotCartesianCoords(tle1, orbitalPositionsTuple1, tle2, orbitalPositionsTuple2):
-    orbitalPositionsX1, orbitalPositionsY1 = orbitalPositionsTuple1
-    orbitalPositionsX2, orbitalPositionsY2 = orbitalPositionsTuple2
-
-    axisRange = 1.5e7
-
-    fig, (ax1, ax2) = plt.subplots(1, 2)
-    fig.set_figheight(5)
-    fig.set_figwidth(11)
-    ax1.set_aspect(1)
-    ax2.set_aspect(1)
-    ax1.set_xlim(-axisRange, axisRange)
-    ax1.set_ylim(-axisRange, axisRange)
-    ax2.set_xlim(-axisRange, axisRange)
-    ax2.set_ylim(-axisRange, axisRange)
-    ax1.title.set_text(tle1.name)
-    ax1.scatter(orbitalPositionsX1, orbitalPositionsY1)
-    ax1.grid(b=True, which='both', axis='both')
-    ax2.title.set_text(tle2.name)
-    ax2.scatter(orbitalPositionsX2, orbitalPositionsY2)
-    ax2.grid(b=True, which='both', axis='both')
-    plt.show()
-
-# plots the distance to the orbital body over time to confirm it is sinusoidal
-
-
-def plot_data(dtcb):
-    fig, ax = plt.subplots(1, 1)
-    ax.scatter(range(0, dtcb.__len__()), dtcb)
-    plt.show()
-
-
-truea1 = ttte.calc_truea_time(tleData.tle1, 120, 5760 * 6)
-truea2 = ttte.calc_truea_time(tleData.tle2, 120, 5760 * 6)
-plotCartesianCoords(tleData.tle1, cartesianCoordsTime(
-    tleData.tle1, truea1), tleData.tle2, cartesianCoordsTime(tleData.tle2, truea2))
-# plot_data(dtcb)
---
+++ experimental_code/testGraphs.py
@@ -0,0 +1,71 @@
+import tleToTrueAnomaly          as ttte
+import keplerToCartesianOrbital  as ktco
+import cartOrbitalToInertial     as coti
+import inertialToRotationalFrame as itrf
+import tleData                   as tled
+
+from math import *
+import matplotlib.pyplot as plt
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# tleToTrueAnomaly
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Test code to plot points over time to make sure the functions work correctly
+def plot_tle_over_time():
+    epochX, trueaY = ttte.calc_truea_time(tled.tle2, 120, 5760 * 6)
+    plt.plot(epochX, trueaY, label="true anomaly")
+    plt.legend()
+    plt.show()
+
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# keplerToCartesianOrbital
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Test code to plot points over time to make sure the functions work correctly
+def plotCartesianCoords(tle1, orbitalPositionsTuple1, tle2, orbitalPositionsTuple2):
+    orbitalPositionsX1, orbitalPositionsY1 = orbitalPositionsTuple1
+    orbitalPositionsX2, orbitalPositionsY2 = orbitalPositionsTuple2
+
+    axisRange = 1.5e7
+
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig.set_figheight(5)
+    fig.set_figwidth(11)
+    ax1.set_aspect(1)
+    ax2.set_aspect(1)
+    ax1.set_xlim(-axisRange, axisRange)
+    ax1.set_ylim(-axisRange, axisRange)
+    ax2.set_xlim(-axisRange, axisRange)
+    ax2.set_ylim(-axisRange, axisRange)
+    ax1.title.set_text(tle1.name)
+    ax1.scatter(orbitalPositionsX1, orbitalPositionsY1)
+    ax1.grid(b=True, which='both', axis='both')
+    ax2.title.set_text(tle2.name)
+    ax2.scatter(orbitalPositionsX2, orbitalPositionsY2)
+    ax2.grid(b=True, which='both', axis='both')
+    plt.show()
+
+# plots the distance to the orbital body over time to confirm it is sinusoidal
+def plot_data(dtcb):
+    fig, ax = plt.subplots(1, 1)
+    ax.scatter(range(0, dtcb.__len__()), dtcb)
+    plt.show()
+
+
+
+
+def main() :
+    plot_tle_over_time()
+
+
+
+    truea1 = ttte.calc_truea_time(tled.tle1, 120, 5760 * 6)
+    truea2 = ttte.calc_truea_time(tled.tle2, 120, 5760 * 6)
+    plotCartesianCoords(tled.tle1, ktco.cartesianCoordsTime(tled.tle1, truea1), tled.tle2, ktco.cartesianCoordsTime(tled.tle2, truea2))
+
+
+
+main()
