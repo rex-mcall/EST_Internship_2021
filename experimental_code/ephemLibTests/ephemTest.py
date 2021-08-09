94e8ab5f53704fce965c1360079b83dbf75a17e9
rex-mcall <rexmcall@gmail.com>
2021-08-09 17:41:42 -0400

    Ephem test works now

--- experimental_code/ephemLibTests/ephemTest.py
+++ experimental_code/ephemLibTests/ephemTest.py
@@ -1,9 +1,31 @@
 import ephem
-import datetime
+import datetime as dt
 import gps
-import tleData as tled
+
+
+tle_string3 = """
+STARLINK-1558           
+1 46035C 20055J   21221.47051397  .00004288  00000-0  28724-3 0  2210
+2 46035  53.0547  78.0035 0001839  85.6035 232.0384 15.06402484    12
+"""
+
+tle_lines = tle_string3.strip().splitlines()
+satellite = ephem.readtle(tle_lines[0], tle_lines[1], tle_lines[2])
 
 observer = ephem.Observer()
-observer.lat = gps.latitude
-observer.lon = gps.longitude
+#convert to Angle type by multiplying ephem.degree
+observer.lat = gps.latitude * ephem.degree
+observer.lon = gps.longitude * ephem.degree
+observer.elev = 13
+observer.date = dt.datetime(2021, 7, 28, 19, 24, 24)
+
+satellite.compute(observer)
+
+
+# satellite.alt is a special Angle type. Multuiplying by 180/pi converts to normal degrees
+print("alt = ", satellite.alt)
+print("az  = ", satellite.az )
+print("----")
+print("alt = ", satellite.alt * (180 / 3.14))
+print("az  = ", satellite.az  * (180 / 3.14))
 
--- experimental_code/ephemLibTests/gps.py
+++ experimental_code/ephemLibTests/gps.py
@@ -1,5 +1,5 @@
 #annapolis
 latitude = 38.9784
-# longitude = -76.4922
-longitude = 283.5078
+longitude = -76.4922
+# longitude = 283.5078
 height = 13
