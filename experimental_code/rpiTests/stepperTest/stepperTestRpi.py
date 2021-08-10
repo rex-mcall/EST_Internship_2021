4cb5dfc5dbe1359cee7b9312b73d5e761467b535
rex-mcall <rexmcall@gmail.com>
2021-08-10 11:02:58 -0400

    Pi stepper tests

---
+++ experimental_code/rpiTests/stepperTest/stepperTestRpi.py
@@ -0,0 +1,34 @@
+from time import sleep
+import RPi.GPIO as GPIO
+
+DIR = 20   # Direction GPIO Pin
+STEP = 21  # Step GPIO Pin
+CW = 1     # Clockwise Rotation
+CCW = 0    # Counterclockwise Rotation
+SPR = 200   # Steps per Revolution (360 / 1.8)
+
+secondsPerRev = 1
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(DIR, GPIO.OUT)
+GPIO.setup(STEP, GPIO.OUT)
+GPIO.output(DIR, CW)
+
+step_count = SPR
+delay = 1 / (SPR / 2)
+
+for x in range(step_count):
+    GPIO.output(STEP, GPIO.HIGH)
+    sleep(delay)
+    GPIO.output(STEP, GPIO.LOW)
+    sleep(delay)
+
+sleep(.5)
+GPIO.output(DIR, CCW)
+for x in range(step_count):
+    GPIO.output(STEP, GPIO.HIGH)
+    sleep(delay)
+    GPIO.output(STEP, GPIO.LOW)
+    sleep(delay)
+
+GPIO.cleanup()
