45e0a7261a34c2cb4f90841b96d9a77030a3e874
mca.rpi <rexmcall@gmail.com>
2021-08-11 16:17:12 -0400

    Fixed error when crossing 360-->0 azimuth and added documentation

--- experimental_code/rpiTests/integratedTests/integratedStepperAltAz.py
+++ experimental_code/rpiTests/integratedTests/integratedStepperAltAz.py
@@ -5,21 +5,28 @@ import ephem
 import datetime as dt
 from datetime import datetime, timezone
 from math import *
+
+# calculated constants to convert to and from radians
 toDeg = 180 / pi
 toRad = pi / 180
 
+
+#tle of the current satellite to track
 tle_string = """
-STARLINK-1667           
-1 46155U 20057AQ  21222.47448513  .00000364  00000-0  43289-4 0  9993
-2 46155  53.0544 223.4919 0000531  43.8846 316.2185 15.06387244 55822
+STARLINK-1274           
+1 45387U 20019AD  21222.35743337 -.00000893  00000-0 -41067-4 0  9997
+2 45387  53.0555  94.0227 0002062  68.5121 291.6088 15.06398712 77942
 """
 
+# parse tle data and initialize satellite as an EarthSatellite object
 tle_lines = tle_string.strip().splitlines()
 satellite = ephem.readtle(tle_lines[0], tle_lines[1], tle_lines[2])
 
+# annapolis, md lat/long
 latitude = 38.9784
 longitude = -76.4922
 
+# sets up reveiver locaiton
 observer = ephem.Observer()
 #convert to Angle type by multiplying ephem.degree
 observer.lat = latitude * ephem.degree
@@ -27,27 +34,32 @@ observer.lon = longitude * ephem.degree
 observer.elev = 13
 observer.date = datetime.now(timezone.utc)
 
-DIR_Elev_Pin = 20   # Direction GPIO Pin
-STEP_Elev_Pin = 21  # Step GPIO Pin
-elevDegPerStep = 0.9
+DIR_Elev_Pin = 20   # Direction GPIO Pin Elevation motor
+STEP_Elev_Pin = 21  # Step GPIO Pin Elevation
 
-DIR_Az_Pin = 2
-STEP_Az_Pin = 3
-azDegPerStep = 0.36
+DIR_Az_Pin = 2 # Direction GPIO Pin Azimuth motor
+STEP_Az_Pin = 3 # Step GPIO Pin Azimuth motor
 
+
+# initializing stepper motor pins
 GPIO.setmode(GPIO.BCM)
-
 GPIO.setup(DIR_Elev_Pin, GPIO.OUT)
 GPIO.setup(STEP_Elev_Pin, GPIO.OUT)
-
 GPIO.setup(DIR_Az_Pin, GPIO.OUT)
 GPIO.setup(STEP_Az_Pin, GPIO.OUT)
 
-currXAngle = 0
-currYAngle = 0
+# initializes variables to remember the current angle of the azimuth and elevation
+currStepperAzimuth = 0
+currStepperElevation = 0
 
-stepDelay = 0.01
+# geared down ratios for degrees per step
+azDegPerStep = 0.36 / 4 # 1:5 @ 1/4 step
+elevDegPerStep = 0.9 # 1:2 at full step
 
+# amount of time the step pulse is high and low
+stepDelay = 0.00001
+
+# defines how to drive the elevation stepper
 def singleStep_Elev(direction):
     GPIO.output(DIR_Elev_Pin, direction)
     GPIO.output(STEP_Elev_Pin, GPIO.HIGH)
@@ -55,6 +67,7 @@ def singleStep_Elev(direction):
     GPIO.output(STEP_Elev_Pin, GPIO.LOW)
     sleep(stepDelay)
 
+#defines how to drive the azimuth stepper
 def singleStep_Az(direction):
     GPIO.output(DIR_Az_Pin, direction)
     GPIO.output(STEP_Az_Pin, GPIO.HIGH)
@@ -62,6 +75,7 @@ def singleStep_Az(direction):
     GPIO.output(STEP_Az_Pin, GPIO.LOW)
     sleep(stepDelay)
 
+# gathers data before checking if the satellite is currently visible
 satellite.compute(observer)
 nextPass = observer.next_pass(satellite, singlepass=False)
 riseTime = nextPass[0].datetime()
@@ -71,35 +85,50 @@ setAzimuth = nextPass[5] * toDeg
 setTime = nextPass[4].datetime()
 
 
-
+# checks to see if the satellite is currently in the sky and informs the user of how long until the satellite is in the sky
 if datetime.utcnow() < riseTime and riseTime < setTime:
     timeTillRise = riseTime - datetime.utcnow()
     secondsToWait = timeTillRise.total_seconds()
     print("Waiting ", secondsToWait / 60 , " minutes till satellite rise.")
     sleep(secondsToWait + 5)
 
+# sets up the satellite data
 observer.date = datetime.now(timezone.utc)
 satellite.compute(observer)
 
 while (satellite.alt * toDeg) >= 0 :
+    # refreshes the satellite position with the current time
     observer.date = datetime.now(timezone.utc)
     satellite.compute(observer)
 
-    elevErrDelta = (satellite.alt * toDeg) - currYAngle # <0 motor too high, >0 motor too low
+    # calculates the angle to the correct elevation
+    elevErrDelta = (satellite.alt * toDeg) - currStepperElevation # <0 motor too high, >0 motor too low
 
+    # pulses the elevation stepper motor in the correct direction
     if elevErrDelta >= (elevDegPerStep * 3/2):
-        print("ystep+")
         singleStep_Elev(1)
-        currYAngle = currYAngle + elevDegPerStep
+        currStepperElevation = currStepperElevation + elevDegPerStep
     elif elevErrDelta <= (-elevDegPerStep * 3/2):
-        print("ystep-")
         singleStep_Elev(0)
-        currYAngle = currYAngle - elevDegPerStep
+        currStepperElevation = currStepperElevation - elevDegPerStep
 
-    # if (currXAngle % 360) < (satellite.az * toDeg) + ((azDegPerStep + 0.1)) :
-    #     singleStep_Az(1)
-    #     currXAngle = currXAngle + azDegPerStep
-    # elif (currXAngle % 360) >= (satellite.az * toDeg) - ((azDegPerStep + 0.1)) :
-    #     singleStep_Az(0)
-    #     currXAngle = currXAngle - azDegPerStep
+    # determines the most efficient direction to get to the correct azimuth
+    azErrDelta = 0
+    currSatAzDeg = satellite.az * toDeg
+    if abs((currSatAzDeg) - currStepperAzimuth) < abs(((currSatAzDeg) + 360) - currStepperAzimuth) and abs((currSatAzDeg) - currStepperAzimuth) < abs(((currSatAzDeg) - 360) - currStepperAzimuth):
+        azErrDelta = (satellite.az * toDeg) - currStepperAzimuth
+    elif abs(((currSatAzDeg) + 360) - currStepperAzimuth) < abs(((currSatAzDeg) - 360) - currStepperAzimuth):
+        azErrDelta = ((satellite.az * toDeg) + 360) - currStepperAzimuth
+    else:
+        azErrDelta = ((currSatAzDeg) - 360) - currStepperAzimuth
+
+    # pulses the azimuth stepper motor in the correct direction
+    if azErrDelta >= (azDegPerStep * 3/2):
+        singleStep_Az(1)
+        currStepperAzimuth = currStepperAzimuth + azDegPerStep
+    elif azErrDelta <= (-azDegPerStep * 3/2) :
+        singleStep_Az(0)
+        currStepperAzimuth = currStepperAzimuth - azDegPerStep
+
+# release GPIO pins back to the Pi
 GPIO.cleanup()
