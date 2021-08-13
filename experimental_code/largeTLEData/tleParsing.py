69d239288188656b48588dd41cf674aca75df6ed
rex-mcall <rexmcall@gmail.com>
2021-08-13 10:47:29 -0400

    parsing large tle data set

---
+++ experimental_code/largeTLEData/tleParsing.py
@@ -0,0 +1,33 @@
+
+
+import ephem
+import datetime as dt
+from datetime import datetime, timezone
+from math import *
+
+# calculated constants to convert to and from radians
+toDeg = 180 / pi
+toRad = pi / 180
+
+# annapolis, md lat/long
+latitude = 38.9784
+longitude = -76.4922
+
+# sets up reveiver locaiton
+observer = ephem.Observer()
+#convert to Angle type by multiplying ephem.degree
+observer.lat = latitude * ephem.degree
+observer.lon = longitude * ephem.degree
+observer.elev = 13
+observer.date = datetime.now(timezone.utc)
+
+x = open("experimental_code\\largeTLEData\\tleData.txt")
+data = x.read().splitlines()
+satTLEs = []
+currIndex = 0
+for line in data:
+    if currIndex % 3 == 0:
+        currSatTLE = [data[currIndex], data[currIndex + 1], data[currIndex + 2]]
+        satTLEs.append(currSatTLE)
+    currIndex = currIndex + 1
+print(x)
--- experimental_code/rpiTests/webUITest/webTest.py
+++ experimental_code/rpiTests/webUITest/webTest.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from flask import request
+import sqlite3
 
 import time
 
 webApp = Flask(__name__)
 
-@app.route("/")
+@webApp.route("/")
 def web_interface():
-    html = open("webTest.html")
+    html = open("experimental_code\\rpiTests\\webUITest\\webTest.html")
     response = html.read().replace('\n', '')
     html.close()
     return response
 
+def main():
+    webApp.run(host= "0.0.0.0")
+
+main()
