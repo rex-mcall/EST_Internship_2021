d586b78a57e1cf6458f5bcd1aa41a6a747c4fc20
rex-mcall <rexmcall@gmail.com>
2021-08-24 22:35:45 -0400

    Testing on the Pi

--- project_code/satDriver.py
+++ project_code/satDriver.py
@@ -148,7 +148,7 @@ class motorInterface():
         azHomed = False
         elevHomed = True
 
-        while not azHomed and not elevHomed:
+        while azHomed != True and elevHomed != True:
             print("in homing loop")
             if azStartedHome and not GPIO.input(az_limit_pin): # if it began by blocking the sensor and hasn't cleared it yet
                 self.singleStep_Az(0)
