102e1d2e4dbb34195a2b5c9af4fbe44cb8b92c8c
rex-mcall <rexmcall@gmail.com>
2021-08-26 21:45:29 -0400

    Testing GPS

--- experimental_code/rpiTests/neo6mTest/gpsTest.py
+++ experimental_code/rpiTests/neo6mTest/gpsTest.py
@@ -7,14 +7,11 @@ port="/dev/ttyAMA0"
 ser=serial.Serial(port, baudrate=9600, timeout=0.5)
 while True:
     newdata=ser.readline()
-    try:
-        pass
-        #print(newdata.decode())
-    except:
-        pass
-    if newdata.decode()[0:6] == '$GPGGA':
+    data = newdata.decode()
+
+    if data[0:6] == '$GPGGA':
         print("in if")
-        newmsg = pynmea2.parse(newdata)
+        newmsg = pynmea2.parse(data)
         lat = newmsg.latitude
         lng = newmsg.longitude
         gps = "Latitude = " + str(lat) + "and Longitude = " + str(lng)
