1835a9f5009d5f96008e23a746b0ded7d8777bd7
rex-mcall <rexmcall@gmail.com>
2021-08-26 22:17:54 -0400

    Testing time set

---
+++ experimental_code/rpiTests/neo6mTest/setSysTime.py
@@ -0,0 +1,14 @@
+import datetime as dt
+import os
+import sys
+
+time  = dt.datetime(2021, 8, 27, 4, 7, 20)
+yr = ((str)(time.year)))
+mon = ((str)(time.month)).zfill(2)
+day = ((str)(time.day)).zfill(2)
+
+hr  = ((str)(time.hour)).zfill(2)
+min = ((str)(time.minute)).zfill(2)
+sec = ((str)(time.second)).zfill(2)
+systemutc = time.year + time.month + time.day + ' ' + hr + ':' + min + ':' + sec
+os.system('sudo date -u --set="%s"' % systemutc)
