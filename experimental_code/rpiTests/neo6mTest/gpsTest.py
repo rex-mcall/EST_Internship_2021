d7d8632555fcc0795c6369a23a030a93742b4023
rex-mcall <rexmcall@gmail.com>
2021-08-26 20:55:31 -0400

    Testing GPS

--- experimental_code/rpiTests/neo6mTest/gpsTest.py
+++ experimental_code/rpiTests/neo6mTest/gpsTest.py
@@ -3,12 +3,12 @@ import time
 import string
 import pynmea2
 
+port="/dev/ttyAMA0"
+ser=serial.Serial(port, baudrate=9600, timeout=0.5)
+dataout = pynmea2.NMEAStreamReader()
 while True:
-    port="/dev/ttyAMA0"
-    ser=serial.Serial(port, baudrate=9600, timeout=0.5)
-    dataout = pynmea2.NMEAStreamReader()
     newdata=ser.readline()
-    if "$GPRMC" in (str)(newdata) :
+    if newdata[0:6] == "$GPRMC":
         newmsg=pynmea2.parse(newdata)
         lat=newmsg.latitude
         lng=newmsg.longitude
